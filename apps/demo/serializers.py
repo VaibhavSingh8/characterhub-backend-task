4b56617ef0800f27a28ecc61d0f0d3e740d0cc44
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2024-12-11 01:34:57 +0530

    serializers and views

--- apps/demo/serializers.py
+++ apps/demo/serializers.py
@@ -1,1 +1,16 @@
 # TODO There's certainly more than one way to do this task, so take your pick.
+
+from rest_framework import serializers
+from .models import Post, Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'text', 'timestamp', 'user']
+
+class PostSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many = True, read_only = True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'text', 'timestamp', 'user', 'comments']
--- apps/demo/views.py
+++ apps/demo/views.py
@@ -1,1 +1,17 @@
 # TODO There's certainly more than one way to do this task, so take your pick.
+
+from rest_framework import viewsets
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.response import Response
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-timestamp')
+    serializer_class = PostSerializer
+
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        return response
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
